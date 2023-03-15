9397af7c70ec588fe6aa9c204959bc3e8aa746b8
Anup Chenthamarakshan <anup@prodvana.io>
2023-03-14 21:53:21 -0700

    Service ID

--- .github/ansible/deploy.sh
+++ .github/ansible/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -eux
+#
 # Inputs:
-# DOCKER_IMAGE: Full docker image in "repository:version" format
-# SERVICE_NAME: Service name. Can be used to pick a subset of processes/hosts to deploy.
-# SERVICE_VERSION: Opaque service version string. This needs to be sent back in get_state.sh
+# PVN_DOCKER_IMAGE: Full docker image in "repository:version" format
+# PVN_SERVICE_NAME: Human readable service name. Can be used to pick a subset of processes/hosts to deploy.
+# PVN_SERVICE_VERSION: Opaque service version string. This needs to be sent back in get_state.sh
+# PVN_SERVICE_ID: Prodvana service id. Used to tag and uniquely identify a service. This variable is passed in get_state.sh.
 
-./get_binaries.sh
+DOCKER_TAG=${PVN_DOCKER_IMAGE} ./get_binaries.sh
 
-ansible-playbook -v deploy.yaml -i prodvana.us-west-2.hosts.yaml -e @ssm_config -e pvn_service_version="${SERVICE_VERSION}" -e pvn_service="${SERVICE_NAME}" >&2
+ansible-playbook -v deploy.yaml -i prodvana.us-west-2.hosts.yaml -e @ssm_config -e pvn_service_version="${PVN_SERVICE_VERSION}" -e pvn_service_id="${PVN_SERVICE_ID}" >&2
--- .github/ansible/get_state.sh
+++ .github/ansible/get_state.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -eux
+#
+# Inputs:
+# PVN_SERVICE_ID: Prodvana service id.
 
 rm -rf /tmp/versions # TODO: Use mktemp -d
 mkdir /tmp/versions
 
-ansible-playbook -v get_state.yaml -i prodvana.us-west-2.hosts.yaml -e @ssm_config -e pvn_service=neon-omnibus >&2
+ansible-playbook -v get_state.yaml -i prodvana.us-west-2.hosts.yaml -e @ssm_config -e pvn_service_id="${PVN_SERVICE_ID}" >&2
 
 hosts=$(ansible-inventory -i prodvana.us-west-2.hosts.yaml -e @ssm_config storage --list | jq -r '._meta.hostvars | keys')
 
 versions="{}"
-for f in /tmp/versions/*; do
+for f in $(find /tmp/versions/ -name '*.version'); do
 	host=$(basename "${f}");
 	v=$(cat "${f}")
 	versions=$(jq -n --arg versions "$versions" --arg key "$host" --arg value "$v" '$versions | fromjson + { ($key) : $value }')
